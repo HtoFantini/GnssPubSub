7159623646a8a035307234681e33ee4e8ebf1953
Henrique Fantini <henrique@harpia.tec.br>
2025-02-07 16:48:51 -0300

    refactor: use function get instead of private variable

--- tests/test_correct_device_creation.py
+++ tests/test_correct_device_creation.py
@@ -14,12 +14,13 @@ def test_gpsreader_creates_Neo6M_device():
 
     gps_reader = GPSReader(publisher, **config)
 
-    # TODO acesso a método privado indevido
-    gps_reader._stop_flag.set()
+    gps_reader.stop_thread()
 
     gps_reader.target_read_loop()
 
-    assert gps_reader._config["device"] == dev_name
+    reader_config = gps_reader.get_configs()
+
+    assert reader_config["device"] == dev_name
 
     dev_class = getattr(gps_devices, dev_name)
     dev_obj = dev_class()

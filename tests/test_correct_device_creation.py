6969424c6a9bea423217d8a87df168ce16a1d635
Henrique Fantini <henrique@harpia.tec.br>
2025-02-07 11:27:04 -0300

    test: assert device creation

--- tests/test_correct_device_creation.py
+++ tests/test_correct_device_creation.py
@@ -1,19 +1,26 @@
 import pytest
 from gpspublisher.reader.reader import GPSReader
 from gpspublisher.publisher.publisher import GPSPublisher
-from gpspublisher.devices.gps_devices import Neo6M  # Importa a classe real do dispositivo
+from gpspublisher.devices import gps_devices
 
 
-def test_gpsreader_creates_neo6m():
-    """Testa se o GPSReader instancia corretamente um objeto Neo6M"""
+def test_gpsreader_creates_Neo6M_device():
+    """
+    Testa se o GPSReader cria corretamente o objeto `dev_obj` com base no nome do dispositivo no config
+    """
+
     publisher = GPSPublisher()
     config = {"read_time": 1, "device": "Neo6M"}
 
     gps_reader = GPSReader(publisher, **config)
 
-    # Chamamos manualmente a função para testar a criação do objeto
+    gps_reader._stop_flag.set()
+
     gps_reader.target_read_loop()
 
-    # Verifica se o objeto foi instanciado corretamente
     assert isinstance(gps_reader._config["device"], str)
     assert gps_reader._config["device"] == "Neo6M"
+
+    dev_class = getattr(gps_devices, "Neo6M")
+    dev_obj = dev_class()
+    assert isinstance(dev_obj, gps_devices.Neo6M)

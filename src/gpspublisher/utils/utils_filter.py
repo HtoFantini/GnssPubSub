4471f5dd07fece364313bd78069cf0e77a3c98b4
Pedro Lopes Chaves <pedro.araca@gmail.com>
2025-02-10 17:05:13 -0300

    refactor: avoid built-in calls like dict

--- src/gpspublisher/utils/utils_filter.py
+++ src/gpspublisher/utils/utils_filter.py
@@ -27,6 +27,6 @@ if __name__ == "__main__":
              "$GPRMC,123519,A,4807.038,N,01131.000,E"
              ",022.4,084.4,230394,003.1,W*6A")
 
-    dict = filter_raw_nmea(lines)
+    dict_out = filter_raw_nmea(LINES)
 
-    print(dict)
+    print(dict_out)
